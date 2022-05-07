691a8e181d1ec5d33f583f7fecf35af546d34094
iwasaki501 <a.iwasaki0128@gmail.com>
2022-05-08 08:45:18 +0900

    modify modint

--- src/math/modint.cc
+++ src/math/modint.cc
@@ -7,125 +7,144 @@
 using int64 = long long;
 
 // snippet-begin
-template <class T>
-T inverse(T a, T m) {
-  T u = 0, v = 1;
-  while (a != 0) {
-    T t = m / a;
-    m -= t * a;
-    std::swap(a, m);
-    u -= t * v;
-    std::swap(u, v);
-  }
-  assert(m == 1);
-  return u;
-}
-
-template <int Mod>
+template <typename T>
 class Modular {
  public:
-  constexpr Modular(int64 val = 0) : val_(val % mod()) {
-    if (val_ < 0) val_ += mod();
+  using Type = typename decay<decltype(T::value)>::type;
+
+  constexpr Modular() : value() {}
+  template <typename U>
+  Modular(const U& x) {
+    value = normalize(x);
   }
 
-  const int64& operator()() const noexcept { return val_; }
+  template <typename U>
+  static Type normalize(const U& x) {
+    Type v;
+    if (-mod() <= x && x < mod()) v = static_cast<Type>(x);
+    else v = static_cast<Type>(x % mod());
+    if (v < 0) v += mod();
+    return v;
+  }
 
-  constexpr int mod() const noexcept { return Mod; }
+  const Type& operator()() const { return value; }
+  template <typename U>
+  explicit operator U() const { return static_cast<U>(value); }
+  constexpr static Type mod() { return T::value; }
 
-  constexpr Modular& operator+=(const Modular& other) noexcept {
-    if ((val_ += other.val_) >= mod()) val_ -= mod();
+  Modular& operator+=(const Modular& other) { if ((value += other.value) >= mod()) value -= mod(); return *this; }
+  Modular& operator-=(const Modular& other) { if ((value -= other.value) < 0) value += mod(); return *this; }
+  template <typename U> Modular& operator+=(const U& other) { return *this += Modular(other); }
+  template <typename U> Modular& operator-=(const U& other) { return *this -= Modular(other); }
+  Modular& operator++() { return *this += 1; }
+  Modular& operator--() { return *this -= 1; }
+  Modular operator++(int) { Modular result(*this); *this += 1; return result; }
+  Modular operator--(int) { Modular result(*this); *this -= 1; return result; }
+  Modular operator-() const { return Modular(-value); }
+
+  template <typename U = T>
+  typename enable_if<is_same<typename Modular<U>::Type, int>::value, Modular>::type& operator*=(const Modular& rhs) {
+  #ifdef _WIN32
+    uint64_t x = static_cast<int64_t>(value) * static_cast<int64_t>(rhs.value);
+    uint32_t xh = static_cast<uint32_t>(x >> 32), xl = static_cast<uint32_t>(x), d, m;
+    asm(
+      "divl %4; \n\t"
+      : "=a" (d), "=d" (m)
+      : "d" (xh), "a" (xl), "r" (mod())
+    );
+    value = m;
+  #else
+    value = normalize(static_cast<int64_t>(value) * static_cast<int64_t>(rhs.value));
+  #endif
+    return *this;
+  }
+  template <typename U = T>
+  typename enable_if<is_same<typename Modular<U>::Type, int64_t>::value, Modular>::type& operator*=(const Modular& rhs) {
+    int64_t q = static_cast<int64_t>(static_cast<long double>(value) * rhs.value / mod());
+    value = normalize(value * rhs.value - q * mod());
+    return *this;
+  }
+  template <typename U = T>
+  typename enable_if<!is_integral<typename Modular<U>::Type>::value, Modular>::type& operator*=(const Modular& rhs) {
+    value = normalize(value * rhs.value);
     return *this;
   }
 
-  constexpr Modular& operator-=(const Modular& other) noexcept {
-    if ((val_ -= other.val_) < 0) val_ += mod();
-    return *this;
-  }
+  Modular& operator/=(const Modular& other) { return *this *= Modular(inverse(other.value, mod())); }
 
-  constexpr Modular& operator*=(const Modular& other) noexcept {
-    (val_ *= other.val_) %= mod();
-    if (val_ < 0) val_ += mod();
-    return *this;
-  }
+  template <typename U>
+  friend const Modular<U>& abs(const Modular<U>& v) { return v; }
 
-  constexpr Modular& operator/=(const Modular& other) noexcept {
-    return *this *= Modular(inverse(other.val_, static_cast<int64>(mod())));
-  }
+  template <typename U>
+  friend bool operator==(const Modular<U>& lhs, const Modular<U>& rhs);
 
-  constexpr Modular& operator++() noexcept { return *this += 1; }
+  template <typename U>
+  friend bool operator<(const Modular<U>& lhs, const Modular<U>& rhs);
 
-  constexpr Modular& operator--() noexcept { return *this -= 1; }
-
-  constexpr Modular operator-() const noexcept { return Modular(-val_); }
-
-  friend std::istream& operator>>(std::istream& is, Modular& num) {
-    int64 x;
-    is >> x;
-    num = Modular(x);
-    return is;
-  }
-
-  friend std::ostream& operator<<(std::ostream& os, const Modular& num) {
-    return os << num();
-  }
+  template <typename U>
+  friend std::istream& operator>>(std::istream& stream, Modular<U>& number);
 
  private:
-  int64 val_;
+  Type value;
 };
 
-constexpr int mod = int(1e9) + 7;
-using Mint = Modular<mod>;
+template <typename T> bool operator==(const Modular<T>& lhs, const Modular<T>& rhs) { return lhs.value == rhs.value; }
+template <typename T, typename U> bool operator==(const Modular<T>& lhs, U rhs) { return lhs == Modular<T>(rhs); }
+template <typename T, typename U> bool operator==(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) == rhs; }
 
-Mint operator+(const Mint& lhs, const Mint& rhs) noexcept {
-  return Mint(lhs) += rhs;
-}
+template <typename T> bool operator!=(const Modular<T>& lhs, const Modular<T>& rhs) { return !(lhs == rhs); }
+template <typename T, typename U> bool operator!=(const Modular<T>& lhs, U rhs) { return !(lhs == rhs); }
+template <typename T, typename U> bool operator!=(U lhs, const Modular<T>& rhs) { return !(lhs == rhs); }
 
-Mint operator-(const Mint& lhs, const Mint& rhs) noexcept {
-  return Mint(lhs) -= rhs;
-}
+template <typename T> bool operator<(const Modular<T>& lhs, const Modular<T>& rhs) { return lhs.value < rhs.value; }
 
-Mint operator*(const Mint& lhs, const Mint& rhs) noexcept {
-  return Mint(lhs) *= rhs;
-}
+template <typename T> Modular<T> operator+(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) += rhs; }
+template <typename T, typename U> Modular<T> operator+(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) += rhs; }
+template <typename T, typename U> Modular<T> operator+(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) += rhs; }
 
-Mint operator/(const Mint& lhs, const Mint& rhs) noexcept {
-  return Mint(lhs) /= rhs;
-}
+template <typename T> Modular<T> operator-(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) -= rhs; }
+template <typename T, typename U> Modular<T> operator-(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) -= rhs; }
+template <typename T, typename U> Modular<T> operator-(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) -= rhs; }
 
-bool operator==(const Mint& lhs, const Mint& rhs) noexcept {
-  return lhs() == rhs();
-}
+template <typename T> Modular<T> operator*(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) *= rhs; }
+template <typename T, typename U> Modular<T> operator*(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) *= rhs; }
+template <typename T, typename U> Modular<T> operator*(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) *= rhs; }
 
-bool operator!=(const Mint& lhs, const Mint& rhs) noexcept {
-  return !(lhs() == rhs());
-}
+template <typename T> Modular<T> operator/(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) /= rhs; }
+template <typename T, typename U> Modular<T> operator/(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) /= rhs; }
+template <typename T, typename U> Modular<T> operator/(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) /= rhs; }
 
-template <class T>
-Mint power(const Mint& a, T b) {
+template<typename T, typename U>
+Modular<T> power(const Modular<T>& a, const U& b) {
   assert(b >= 0);
-  Mint x = a, res = 1;
-  while (b) {
-    if (b & 1) res *= x;
+  Modular<T> x = a, res = 1;
+  U p = b;
+  while (p > 0) {
+    if (p & 1) res *= x;
     x *= x;
-    b >>= 1;
+    p >>= 1;
   }
   return res;
 }
 
-// 二項係数テーブルを必要なところまで作る
-// inv だけ前計算したいときは choose(200000, 1) とかで適当に空呼び出しすればいい
-// std::vector<Mint> fact{1, 1};
-// std::vector<Mint> inv{0, 1};
-// std::vector<Mint> inv_fact{1, 1};
+template <typename T>
+bool IsZero(const Modular<T>& number) {
+  return number() == 0;
+}
+ 
+template <typename T>
+std::ostream& operator<<(std::ostream& stream, const Modular<T>& number) {
+  return stream << number();
+}
 
-// Mint choose(int n, int k) noexcept {
-//   if (n < k || n < 0 || k < 0) return 0;
-//   while (int(fact.size()) < n + 1) {
-//     int sz = fact.size();
-//     fact.emplace_back(fact.back() * sz);
-//     inv.emplace_back(mod - inv[mod % sz] * (mod / sz));
-//     inv_fact.emplace_back(inv_fact.back() * inv.back());
-//   }
-//   return fact[n] * inv_fact[k] * inv_fact[n - k];
-// }
+template <typename T>
+std::istream& operator>>(std::istream& stream, Modular<T>& number) {
+  typename common_type<typename Modular<T>::Type, int64_t>::type x;
+  stream >> x;
+  number.value = Modular<T>::normalize(x);
+  return stream;
+}
+
+constexpr int MOD = (int) 1e9 + 7;
+using Mint = Modular<std::integral_constant<std::decay<decltype(MOD)>::type, MOD>>;
 // snippet-end
